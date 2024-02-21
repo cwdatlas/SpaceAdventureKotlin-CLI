bbcb2e6b5bc623be6293263e7a264378641f527b
cwdatlas <ascott@carroll.edu>
2024-02-21 11:36:50 -0700

    added code to start game

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
-package org.example
+import org.example.Controller
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Hello World")
+    Controller().startGame()
+
 }
