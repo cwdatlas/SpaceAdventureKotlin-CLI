c92383489a1eb0d2a1a864c61ac9addce1d58ac0
cwdatlas <ascott@carroll.edu>
2024-02-21 23:05:44 -0700

    updated to perform gradlew run

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("MainKt")
+}
 
 repositories {
     mavenCentral()
